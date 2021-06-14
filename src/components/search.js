ac241d83d04e2c56371b855924875f6297a02bfa
Johan <joh.pik.fr@gmail.com>
2021-06-14 14:04:59 +1000

    refactored search using controlled input instead of useRef

--- src/components/search.js
+++ src/components/search.js
@@ -1,28 +1,29 @@
 /*global chrome*/
-import React, { useEffect, useRef } from 'react';
+import React, { useState } from 'react';
 
 const Search = () => {
-    const refContainer = useRef(null);
+    
+    const [query, setQuery] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
         
-        let query = refContainer.current.value;
-
         chrome.search.query({
             text: query
         });
     };
-    
-    useEffect(() => {
-        refContainer.current.focus(); 
-    });
-    
+
+
     return (
         <>
             <form className='form' onSubmit={handleSubmit}>
                 <div>
-                    <input id="myInput" type='text' ref={refContainer} />
+                    <input 
+                        id="myInput" 
+                        type='text' 
+                        value={query} 
+                        onChange={ e => setQuery(e.target.value)}
+                    />
                 </div>
             </form>
         </>
@@ -30,3 +31,39 @@ const Search = () => {
 };
 
 export default Search;
+
+
+/* OLD Version using useRef*/
+// import React, { useEffect, useRef } from 'react';
+
+// const Search = () => {
+//     const refContainer = useRef(null);
+
+//     const handleSubmit = (e) => {
+//         e.preventDefault();
+
+//         let query = refContainer.current.value;
+
+//         chrome.search.query({
+//             text: query
+//         });
+
+//         refContainer.current.value = '';
+//     };
+
+//     useEffect(() => {
+//         refContainer.current.focus();
+//     });
+
+//     return (
+//         <>
+//             <form className='form' onSubmit={handleSubmit}>
+//                 <div>
+//                     <input id="myInput" type='text' ref={refContainer} />
+//                 </div>
+//             </form>
+//         </>
+//     );
+// };
+
+// export default Search;
