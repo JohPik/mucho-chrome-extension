386a49226ad40015b847238f21172b1f71c230e8
Johan <joh.pik.fr@gmail.com>
2021-06-20 18:22:10 +1000

    Generates Quotes from API

--- src/components/bookmarks.js
+++ src/components/bookmarks.js
@@ -6,7 +6,7 @@ export default function Bookmarks() {
     return (
         <div className="bookmarks">
 
-            <div class="bookmarks__navigation">
+            <div className="bookmarks__navigation">
                 <ul>
                     <li>home</li>
                     <li>shopping</li>
@@ -14,7 +14,7 @@ export default function Bookmarks() {
                     <li>streaming</li>
                     <li>entertainment</li>
                 </ul>
-                <button class="bookmarks__navigation__edit-button"></button>
+                <button className="bookmarks__navigation__edit-button"></button>
             </div>
 
             <div className="bookmarks__links">
--- src/components/quote.js
+++ src/components/quote.js
@@ -1,13 +1,40 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 export default function Quote() {
+
+    const [quote, setQuote] = useState("");
+    const [author, setAuthor] = useState("");
+
+    const getQuote = () => {
+        fetch("https://api.quotable.io/random")
+        .then(res => res.json())
+        .then(data => {
+            const { content: quote, author } = data;
+            console.log(data);
+            setQuote(quote);
+            setAuthor(author);
+        })
+        .catch( err => console.error(err))
+    };
+
+    useEffect( () => {
+        getQuote();
+    }, [])
+
     return (
-        <section class="quote">
+        <section className="quote">
             <figure>
                 <blockquote cite="#">
-                    <p>Imagination leads the world.</p>
+                    <p>{quote}</p>
                 </blockquote>
-                <figcaption>—Napoleon Bonaparte, <cite>Quote API</cite></figcaption>
+                <figcaption>—{author}, 
+                    <cite>
+                        <a href="https://github.com/lukePeavey/quotable" target="_blank" rel="noopener noreferrer">
+                            Quotable
+                        </a>
+                    </cite>
+                </figcaption>
             </figure>
         </section>
 
