2ad17566f853c6e783a9a7783e1cd084ec182059
Johan <joh.pik.fr@gmail.com>
2021-07-10 11:18:50 +1000

    Create a utility to mimic Tabs objec

---
+++ src/utilities.js
@@ -0,0 +1,89 @@
+const tabs = [
+    {
+        id: 1,
+        name: "home",
+        links: [
+            {
+                id: 1,
+                linkName: "amazon",
+                url: "#"
+            },
+            {
+                id: 2,
+                linkName: "netflix",
+                url: "#"
+            },
+            {
+                id: 3,
+                linkName: "youtube",
+                url: "#"
+            }
+        ]
+    },
+    {
+        id: 2,
+        name: "shopping",
+        links: [
+            {
+                id: 1,
+                linkName: "amazon",
+                url: "#"
+            },
+            {
+                id: 2,
+                linkName: "ebay",
+                url: "#"
+            }
+        ]
+    },
+    {
+        id: 3,
+        name: "sports",
+        links: [
+            {
+                id: 1,
+                linkName: "footbal",
+                url: "#"
+            },
+            {
+                id: 2,
+                linkName: "tennis",
+                url: "#"
+            }
+        ]
+    },
+    {
+        id: 4,
+        name: "streaming",
+        links: [
+            {
+                id: 1,
+                linkName: "Netflix",
+                url: "#"
+            },
+            {
+                id: 2,
+                linkName: "Prime",
+                url: "#"
+            }
+        ]
+    },
+    {
+        id: 5,
+        name: "Entertainment",
+        links: [
+            {
+                id: 1,
+                linkName: "Youtube",
+                url: "#"
+            },
+            {
+                id: 2,
+                linkName: "Spotify",
+                url: "#"
+            }
+        ]
+    },
+];
+
+export default tabs;
