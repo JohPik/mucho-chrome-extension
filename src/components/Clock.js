2c6417cc7a89d1b7a7f79c6796a2f11e2cb9b880
Johan <joh.pik.fr@gmail.com>
2021-07-25 17:16:30 +1000

    Toggle from 24 to 12 time format

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import { useGlobalContext } from '../context';
 
 export default function Clock() {
+    
+    const { settingsState } = useGlobalContext();
+    const { timeFormat } = settingsState;
 
     const [time, setTime] = useState({});
 
@@ -18,13 +22,25 @@ export default function Clock() {
         return () => clearInterval(interval);
     }, []);
 
-    return (
-        <>
-            {time.hours &&
+    if (time.hours) {
+        if (timeFormat === "24") {
+            return (
                 <p className="time-live" >
                     {time.hours <= 9 && 0}{time.hours}:{time.minutes <= 9 && 0}{time.minutes}:{time.seconds <= 9 && 0}{time.seconds}
                 </p>
-            }
+            )
+        } else if (timeFormat === "12") {
+            return (
+                <p className="time-live" >
+                    
+                    {time.hours <= 9 && 0}{time.hours >= 12 ? time.hours - 12 : time.hours}:{time.minutes <= 9 && 0}{time.minutes}:{time.seconds <= 9 && 0}{time.seconds} {time.hours >=12 ? "pm" : "am"}
+                </p>
+            )
+        }
+    }
+
+    return (
+        <>
         </>
     )
 };
